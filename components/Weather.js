1acdfb7702c15afe065e39015631cff6ce0eed96
noppasin85 <topzaza_2011@hotmail.com>
2020-09-17 11:37:41 +0700

    weather fix

--- components/Weather.js
+++ components/Weather.js
@@ -4,17 +4,17 @@ import Forecast from './Forecast';
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
-        main: '-',
-        description: '-',
-        temp: 30
+        main: 'main',
+        description: 'description',
+        temp: 0,
     })
     return (
         <View>
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-                <View style={styles.cover}> 
+                <View style={styles.cover}>
                     <Text style={styles.medium}>Zip Code: {props.zipCode}</Text>
                     <Forecast {...forecastInfo} />
-                </View>    
+                </View>
             </ImageBackground>
         </View>
     );
@@ -23,14 +23,15 @@ const styles = StyleSheet.create({
     backdrop: {
         alignItems: 'center',
         width: '100%',
-        height: '100%'
-        
+        height: '100%',
     },
     cover: {
-        backgroundColor: '#111',
+        backgroundColor: '#000', 
         width: '100%',
         height: 300,
         opacity: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     medium: {
         fontSize: 20, 
